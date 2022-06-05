e1e02e5a6ab6ad80270c917c12a64afed977b996
dawid <dpilif@gmail.com>
2022-06-05 11:36:16 +0200

    master: create empty springboot project with kafka.

---
+++ src/main/java/com/df/ExampleKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleKafkaApplication.class, args);
+	}
+
+}
